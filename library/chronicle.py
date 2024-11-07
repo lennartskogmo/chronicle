7a5237db60f13bf73b3d34eb52856cbad3fc49bc
Lennart Skogmo <lennart@skogmo.net>
2024-11-07 14:26:11 +0100

    WIP

--- library/chronicle.py
+++ library/chronicle.py
@@ -2,7 +2,7 @@
 from os import environ
 from re import match, sub
 from urllib.parse import urlencode
-from pyspark.sql.functions import coalesce, col, concat_ws, current_timestamp, expr, lag, lead, lit, md5, row_number, when, xxhash64
+from pyspark.sql.functions import coalesce, col, concat_ws, current_timestamp, expr, hash, lag, lead, lit, md5, row_number, when
 from pyspark.sql.types import StringType
 from pyspark.sql.window import Window
 
@@ -97,7 +97,7 @@ def add_checksum_column(df, ignore=None):
         columns = sorted(c for c in df.columns if c not in ignore)
     else:
         raise Exception("Invalid ignore")
-    return df.select([xxhash64(concat_ws("<|>", *[coalesce(col(c).cast(StringType()), lit('')) for c in columns])).alias(CHECKSUM), "*"])
+    return df.select([hash(concat_ws("<|>", *[coalesce(col(c).cast(StringType()), lit('')) for c in columns])).alias(CHECKSUM), "*"])
 
 # Add KEY column to beginning of data frame.
 def add_key_column(df, key):
