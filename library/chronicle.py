e6f49e4d655e833afd86c3766196a834195fca36
Lennart Skogmo <lennart@skogmo.net>
2024-11-15 11:40:05 +0100

    WIP

--- library/chronicle.py
+++ library/chronicle.py
@@ -2,7 +2,7 @@
 from os import environ
 from re import match, sub
 from urllib.parse import urlencode
-from pyspark.sql.functions import coalesce, col, concat_ws, current_timestamp, expr, lag, lead, lit, md5, row_number, when, xxhash64
+from pyspark.sql.functions import coalesce, col, concat_ws, current_timestamp, expr, lag, lead, lit, lower, md5, row_number, when, xxhash64
 from pyspark.sql.types import StringType
 from pyspark.sql.window import Window
 
@@ -27,6 +27,41 @@ LOADED     = "__loaded"     # The time the record was loaded.
 ANONYMIZED = "__anonymized" # Reserved for the time the record was anonymized.
 
 
+# Return dictionary containing connection configuration or None if no connection was found.
+def get_connection(connection):
+    if isinstance(connection, str):
+        connection = spark.table(CONNECTION).filter(lower("ConnectionName") == lower(lit(connection))).collect()
+        connection = connection[0].asDict() if connection else None
+        return connection
+    else:
+        raise Exception("Invalid connection")
+
+# Return dictionary containing connection configuration including secrets or None if no connection was found.
+def get_connection_with_secrets(connection):
+    if isinstance(connection, str):
+        connection = spark.table(CONNECTION).filter(lower("ConnectionName") == lower(lit(connection))).collect()
+        connection = connection[0].asDict() if connection else None
+        if connection is None:
+            return connection
+    if isinstance(connection, dict):
+        connection_with_secrets = {}
+        for key, value in connection.items():
+            connection_with_secrets[key] = resolve_secret(value)
+        if not connection_with_secrets:
+            connection_with_secrets = None 
+        return connection_with_secrets
+    else:
+        raise Exception("Invalid connection")
+
+# Return dictionary containing object configuration or None if no object was found.
+def get_object(object):
+    if isinstance(object, str):
+        object = spark.table(OBJECT).filter(lower("ObjectName") == lower(lit(object))).collect()
+        object = object[0].asDict() if object else None
+        return object
+    else:
+        raise Exception("Invalid object")
+
 # Return new reader instance if argument is string, else act as pass-through function and return argument itself.
 def get_reader(reader):
     if isinstance(reader, str):
@@ -48,6 +83,13 @@ def get_reader(reader):
         reader = globals()[connection_with_secrets["Reader"]]
         reader = reader(**reader_arguments)
     return reader
+
+# Return secret if value contains reference to secret, otherwise return value.
+def resolve_secret(value):
+    if isinstance(value, str) and value.startswith("<") and value.endswith(">"):
+        return dbutils.secrets.get(scope="kv", key=value[1:-1])
+    else:
+        return value
 
 # Validate mode against dynamic list of values.
 def validate_mode(valid, mode):
