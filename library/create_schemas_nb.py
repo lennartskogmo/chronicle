0bdcbde19fee68d6b7c6810cffbbc883ea7ce3b7
Lennart Skogmo <lennart@skogmo.net>
2025-03-04 20:55:05 +0100

    WIP

--- library/create_schemas_nb.py
+++ library/create_schemas_nb.py
@@ -2,7 +2,7 @@
 # MAGIC %run ./chronicle.py
 
 # COMMAND ----------
-schemas = spark.sql(f"SELECT DISTINCT LOWER(SPLIT(ObjectName, '\\\.')[0]) AS schema_name FROM {OBJECT} WHERE Status = 'Active'")
+df = spark.sql(f"SELECT DISTINCT LOWER(SPLIT(ObjectName, '\\\.')[0]) AS schema_name FROM {OBJECT} WHERE Status = 'Active'")
 
-for row in schemas.rdd.collect():
+for row in df.rdd.collect():
     spark.sql(f"CREATE SCHEMA IF NOT EXISTS {row['schema_name']}")
