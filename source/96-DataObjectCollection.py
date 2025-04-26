a2a8d086acb96e1eae1f4d8c0bbd2cade8d83811
Lennart Skogmo <lennart@skogmo.net>
2025-04-26 17:39:14 +0200

    WIP

---
+++ source/96-DataObjectCollection.py
@@ -0,0 +1,63 @@
+class DataObjectCollection:
+
+    __objects = {}
+
+    # Initialize collection.
+    def __init__(self, objects):
+        if not isinstance(objects, dict):
+            raise Exception("Invalid objects")
+        self.__objects = objects
+        # MAYBE: Validate object class.
+
+    def __getitem__(self, object_name):
+        return self.__objects.get(object_name)
+    
+    def __len__(self):
+        return len(self.__objects)
+    
+    def items(self):
+        return self.__objects.items()
+    
+    def keys(self):
+        return self.__objects.keys()
+
+    def values(self):
+        return self.__objects.values()
+    
+    # Return new subcollection containing only active objects.
+    def active(self):
+        objects = {}
+        for object_name, object in self.__objects.items():
+            if object.Status == "Active":
+                objects[object_name] = object
+        return DataObjectCollection(objects)
+    
+    # Return new subcollection containing only inactive objects.
+    def inactive(self):
+        objects = {}
+        for object_name, object in self.__objects.items():
+            if object.Status == "Inactive":
+                objects[object_name] = object
+        return DataObjectCollection(objects)
+    
+    # Return new subcollection containing only objects with matching connection name.
+    def connection(self, connection_name):
+        if not isinstance(connection_name, str):
+            raise Exception("Invalid connection name")
+        objects = {}
+        for object_name, object in self.__objects.items():
+            if object.ConnectionName == connection_name:
+                objects[object_name] = object
+        return DataObjectCollection(objects)
+
+    # Return new subcollection containing only objects with atleast one matching tag.
+    def tags(self, tags):
+        if isinstance(tags, str):
+            tags = parse_tag(tags)
+        if not isinstance(tags, list):
+            raise Exception("Invalid tags")
+        objects = {}
+        for object_name, object in self.__objects.items():
+            if hasattr(object, "Tags") and any(item in object.Tags for item in tags):
+                objects[object_name] = object
+        return DataObjectCollection(objects)
