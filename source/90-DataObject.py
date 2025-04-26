7abb28aa4494e694848a335830e3ce86f25981c9
Lennart Skogmo <lennart@skogmo.net>
2025-04-26 17:25:07 +0200

    WIP

---
+++ source/90-DataObject.py
@@ -0,0 +1,41 @@
+class DataObject:
+
+    __connection = None
+
+    def __init__(self, configuration):
+        if not isinstance(configuration, dict):
+            raise Exception("Invalid configuration")
+        for key, value in configuration.items():
+            setattr(self, key, value)
+        self.__validate_configuration()
+    
+    def __validate_configuration(self):
+        # Validate mandatory ObjectName.
+        if not hasattr(self, "ObjectName"):
+            raise Exception(f"Missing ObjectName in {vars(self)}")
+        if not isinstance(self.ObjectName, str) or self.ObjectName.strip() == "":
+            raise Exception(f"Invalid ObjectName in {vars(self)}")
+
+        # Validate mandatory ConnectionName.
+        if not hasattr(self, "ConnectionName"):
+            raise Exception(f"Missing ConnectionName in {self.ObjectName}")
+        if not isinstance(self.ConnectionName, str) or self.ConnectionName.strip() == "":
+            raise Exception(f"Invalid ConnectionName in {self.ObjectName}")
+
+        # Validate mandatory Status.
+        if not hasattr(self, "Status"):
+            raise Exception(f"Missing Status in {self.ObjectName}")
+        if not isinstance(self.Status, str) or not self.Status in ["Active", "Inactive"]:
+            raise Exception(f"Invalid Status in {self.ObjectName}")
+
+        # Validate optional Tags.
+        if hasattr(self, "Tags") and not isinstance(self.Tags, list):
+            raise Exception(f"Invalid Tags in {self.ObjectName}")
+
+    def set_connection(self, connection):
+        if self.__connection is not None:
+            raise Exception("Connection already set")
+        self.__connection = connection
+
+    def load(self):
+        print(self.Function)
