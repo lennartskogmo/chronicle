1047ff291f449dfd67bbd3894fd270a4819646b6
Lennart Skogmo <lennart@skogmo.net>
2025-04-26 17:33:37 +0200

    WIP

---
+++ source/90-DataConnection.py
@@ -0,0 +1,10 @@
+class DataConnection:
+
+    __secrets = {}
+
+    # Initialize connection.
+    def __init__(self, configuration):
+        if not isinstance(configuration, dict):
+            raise Exception("Invalid configuration")
+        for key, value in configuration.items():
+            setattr(self, key, value)
