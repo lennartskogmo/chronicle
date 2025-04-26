863c2e1c99ad1600631ff9be0449e3c8f2301fe2
Lennart Skogmo <lennart@skogmo.net>
2025-04-26 23:30:04 +0200

    WIP

--- library/chronicle.py
+++ library/chronicle.py
@@ -847,6 +847,21 @@ class DataConnection:
             raise Exception("Invalid configuration")
         for key, value in configuration.items():
             setattr(self, key, value)
+        self.__validate_configuration()
+
+    # Validate configuration attributes.
+    def __validate_configuration(self):
+        # Validate mandatory ConnectionName.
+        if not hasattr(self, "ConnectionName"):
+            raise Exception(f"Missing ConnectionName in {vars(self)}")
+        if not isinstance(self.ConnectionName, str) or self.ConnectionName.strip() == "":
+            raise Exception(f"Invalid ConnectionName in {vars(self)}")
+
+        # Validate mandatory ConcurrencyLimit.
+        if not hasattr(self, "ConcurrencyLimit"):
+            raise Exception(f"Missing ConcurrencyLimit in {self.ConnectionName}")
+        if not isinstance(self.ConcurrencyLimit, int) or self.ConcurrencyLimit < 1:
+            raise Exception(f"Invalid ConcurrencyLimit in {self.ConnectionName}")
 
 
 class DataConnectionRepository: # [OK]
