bae7fa1bc0542d0eadbc1722e232f6635b8c7813
Lennart Skogmo <lennart@skogmo.net>
2025-04-28 15:44:06 +0200

    WIP

--- library/chronicle.py
+++ library/chronicle.py
@@ -862,7 +862,7 @@ class DataConnection:
             raise Exception(f"Invalid ConcurrencyLimit in {self.ConnectionName}")
 
         # Validate optional Reader.
-        if hasattr(self, "Reader") and not isinstance(self.Reader, str):
+        if hasattr(self, "Reader") and self.Reader is not None and not isinstance(self.Reader, str):
             raise Exception(f"Invalid Reader in {self.ConnectionName}")
 
     # Return dictionary containing configuration with secrets.
@@ -878,6 +878,24 @@ class DataConnection:
                 configuration_with_secrets[key] = get_value
             self.__configuration_with_secrets = configuration_with_secrets
         return self.__configuration_with_secrets
+
+    # Return new reader instance.
+    def get_reader(self):
+        if not hasattr(self, "Reader") or self.Reader is None:
+            raise Exception(f"No reader associated with connection {self.ConnectionName}")
+        configuration_with_secrets = self.__get_configuration_with_secrets()
+        reader = globals()[configuration_with_secrets["Reader"]()]
+        reader_arguments = {}
+        for key, value in configuration_with_secrets.items():
+            if value is not None:
+                if key == "Host"      : reader_arguments["host"]      = value()
+                if key == "Port"      : reader_arguments["port"]      = value()
+                if key == "Database"  : reader_arguments["database"]  = value()
+                if key == "Username"  : reader_arguments["username"]  = value()
+                if key == "Password"  : reader_arguments["password"]  = value()
+                if key == "Warehouse" : reader_arguments["warehouse"] = value()
+        return reader(**reader_arguments)
+
 
     def test(self):
         import pprint
