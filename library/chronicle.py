382f7a579b40d9acc53fecad4d0edf7f08e9c039
Lennart Skogmo <lennart@skogmo.net>
2025-04-28 23:05:02 +0200

    WIP

--- library/chronicle.py
+++ library/chronicle.py
@@ -871,29 +871,29 @@ class DataConnection:
         if not hasattr(self, f"_{self.__class__.__name__}__configuration_with_secrets"):
             configuration_with_secrets = {}
             for key, value in vars(self).items():
-                value = resolve_secret(value)
-                # Wrap value in function.
-                def get_value(value=value):
-                    return value
-                configuration_with_secrets[key] = get_value
+                if value is not None:
+                    value = resolve_secret(value)
+                    # Wrap value in function.
+                    def get_value(value=value):
+                        return value
+                    configuration_with_secrets[key] = get_value
             self.__configuration_with_secrets = configuration_with_secrets
         return self.__configuration_with_secrets
 
     # Return new reader instance.
     def get_reader(self):
         if not hasattr(self, "Reader") or self.Reader is None:
-            raise Exception(f"No reader associated with connection {self.ConnectionName}")
+            raise Exception("No reader associated with connection")
         configuration_with_secrets = self.__get_configuration_with_secrets()
         reader = globals()[configuration_with_secrets["Reader"]()]
         reader_arguments = {}
         for key, value in configuration_with_secrets.items():
-            if value is not None:
-                if key == "Host"      : reader_arguments["host"]      = value()
-                if key == "Port"      : reader_arguments["port"]      = value()
-                if key == "Database"  : reader_arguments["database"]  = value()
-                if key == "Username"  : reader_arguments["username"]  = value()
-                if key == "Password"  : reader_arguments["password"]  = value()
-                if key == "Warehouse" : reader_arguments["warehouse"] = value()
+            if key == "Host"      : reader_arguments["host"]      = value()
+            if key == "Port"      : reader_arguments["port"]      = value()
+            if key == "Database"  : reader_arguments["database"]  = value()
+            if key == "Username"  : reader_arguments["username"]  = value()
+            if key == "Password"  : reader_arguments["password"]  = value()
+            if key == "Warehouse" : reader_arguments["warehouse"] = value()
         return reader(**reader_arguments)
 
 
