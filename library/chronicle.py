1668675f2168f32d5d5b6a8237a945151c03bc0d
Lennart Skogmo <lennart@skogmo.net>
2025-05-20 11:33:27 +0200

    WIP

--- library/chronicle.py
+++ library/chronicle.py
@@ -239,13 +239,29 @@ def load_incremental(
     return writer.write(df)
 
 
+# Return connection.
+def get_connection(connection):
+    repo = DataConnectionRepository()
+    return repo.get_connection(connection)
+
+# Return object.
 def get_object(object):
     repo = DataObjectRepository()
     return repo.get_object(object)
 
+# Return object collection.
 def get_objects():
     repo = DataObjectRepository()
     return repo.get_objects()
+
+# Return reader.
+def get_reader(connection):
+    repo = DataConnectionRepository()
+    connection = repo.get_connection(connection)
+    if connection is not None:
+        return connection.get_reader()
+    else:
+        return None
 
 
 # Delta table batch writer.
