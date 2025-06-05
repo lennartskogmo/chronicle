0c8543586e9ecc5b6631f515be77eb600c4c8b90
Lennart Skogmo <163730026+lennartskogmo@users.noreply.github.com>
2025-06-05 05:06:04 +0200

    Clean up after refactor

--- source/42-DataObject.py
+++ source/42-DataObject.py
@@ -23,11 +23,11 @@ class DataObject:
         if not isinstance(self.ConnectionName, str) or self.ConnectionName.strip() == "":
             raise Exception(f"Invalid ConnectionName in {self.ObjectName}")
 
-        # Validate mandatory Function.
-        if not hasattr(self, "Function"):
-            raise Exception(f"Missing Function in {self.ObjectName}")
-        if not isinstance(self.Function, str) or not self.Function.startswith("load_"):
-            raise Exception(f"Invalid Function in {self.ObjectName}")
+        # Validate mandatory LoadFunction.
+        if not hasattr(self, "LoadFunction"):
+            raise Exception(f"Missing LoadFunction in {self.ObjectName}")
+        if not isinstance(self.LoadFunction, str) or not self.LoadFunction.startswith("load_"):
+            raise Exception(f"Invalid LoadFunction in {self.ObjectName}")
 
         # Validate mandatory Status.
         if not hasattr(self, "Status"):
@@ -75,7 +75,7 @@ class DataObject:
 
     # Load object.
     def load(self):
-        function = globals()[self.Function]
+        function = globals()[self.LoadFunction]
         function_arguments = {}
         # Map object configuration to load function arguments.
         for key, value in vars(self).items():
