6968e849818d4ad06a0c1379d184eb3a951faada
Lennart Skogmo <lennart@skogmo.net>
2025-10-28 13:37:46 +0100

    WIP

--- source/20-DeltaBatchChangeWriter.py
+++ source/20-DeltaBatchChangeWriter.py
@@ -3,14 +3,14 @@ class DeltaBatchChangeWriter:
 
     # Initialize writer.
     def __init__(self, mode, table, key, ignore=None, hash=None, drop=None):
-        validate_mode(mode=mode, valid=["append", "insert_update", "insert_update_delete"])
+        validate_mode(mode=mode, valid=["backfill", "insert_update", "insert_update_delete"])
         self.key    = key
         self.drop   = drop
         self.hash   = hash
         self.mode   = mode
         self.table  = table
         self.ignore = ignore
-    
+
     # Create Delta table with added metadata columns ANONYMIZED, LOADED, OPERATION, CHECKSUM and KEY.
     def __create_table(self, df):
         df = df.select([lit("C").alias(OPERATION), "*"])
@@ -28,7 +28,7 @@ class DeltaBatchChangeWriter:
             dw = dw.option("delta.enableDeletionVectors", "false")
             dw = dw.option("delta.enableRowTracking", "false")
         dw.saveAsTable(self.table)
-    
+
     # Prepare data frame and Delta table for writing.
     def __prepare(self, df):
         # Perform column transformations.
@@ -37,21 +37,20 @@ class DeltaBatchChangeWriter:
         df = add_key_column(df, self.key)                  # 3 Add key column
         if self.drop: df = drop_columns(df, self.drop)     # 4 Drop columns
         df = add_checksum_column(df, self.ignore)          # 5 Add checksum column
-        # Remove duplicates
+        # Put deduplication logic here.
         # Create table.
         if not spark.catalog._jcatalog.tableExists(self.table):
             self.__create_table(df)
             return df, True
         else:
             return df, False
-    
+
     # Write data frame to Delta table.
     def write(self, df):
         df, new = self.__prepare(df)
-        # mode = "append" if new else self.mode
         mode = self.mode
-        if mode == "append":
-            self.__append(df)
+        if mode == "backfill":
+            self.__backfill(df)
         elif mode == "insert_update":
             self.__insert_update(df)
         elif mode == "insert_update_delete":
@@ -59,13 +58,14 @@ class DeltaBatchChangeWriter:
         else:
             raise Exception("Invalid mode")
         return get_num_output_rows(self.table)
-    
-    # Append source data frame.
-    def __append(self, df):
+
+    # Write data frame to Delta table without doing any checks.
+    # Can be used to speed up backfill if source can provide data without overlap and duplicates.
+    def __backfill(self, df):
         df = df.select([lit("I").alias(OPERATION), "*"])
         df = df.select([current_timestamp().alias(LOADED), "*"])
         df.write.format("delta").mode("append").option("mergeSchema", "true").saveAsTable(self.table)
-    
+
     # Capture inserted and updated records from full or partial source data frame.
     # Will only write one row per key even if source data frame contains duplicate rows.
     # Arbitrary row will be used if source data frame contains nonidentical duplicate rows.
@@ -83,7 +83,7 @@ class DeltaBatchChangeWriter:
             "*"]) \
         .drop(col("_origin")).where(col(OPERATION).isNotNull())
         df.write.format("delta").mode("append").option("mergeSchema", "true").saveAsTable(self.table)
-    
+
     # Capture inserted, updated and deleted records from full source data frame.
     # Will only write one row per key even if source data frame contains duplicate rows.
     # Arbitrary row will be used if source data frame contains nonidentical duplicate rows.
